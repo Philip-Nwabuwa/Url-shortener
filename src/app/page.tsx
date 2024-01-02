5546cd9adb22a0c13eee4c7a17f2f7e0720c6e17
Philip John <95682231+Philip-Nwabuwa@users.noreply.github.com>
2024-01-02 02:06:41 +0000

    chore: Added dark theme & ui.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import ModeToggle from "@/components/common/ThemeToggle";
+
 export default function Home() {
-  return <main>Hello World</main>;
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen py-2 dark:bg-gray-900 dark:text-slate-200">
+      <h1>Hello Next.js</h1>
+      <div className="">
+        <ModeToggle />
+      </div>
+    </main>
+  );
 }
